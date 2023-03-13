46c132ceab95191d257b7d3809a6a14abfd46d4b
wwc <onechoo1106@gmail.com>
2023-03-13 17:51:14 +1000

    Stack list view

--- week2/ContentView.swift
+++ week2/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -46,12 +47,48 @@ struct foreach_list_view:View{
         }
     }
 }
+/*
+struct get_data_from_othe:View{
+    var body:some View{
+        /*
+        let file = "data.txt"
+        let text = "text test"
+        if let dir=FileManager.default.urls(for:.documentDirectory,in: .userDomainMask).first{
+            let fileURL = dir.appendingPathComponent(file)
+            do{
+                try text.write(to:fileURL,atomically:false,encoding:.utf8)
+            }catch{
+                print("error occured")
+            }
+            
+        }*/
+        
+    }
+} */ // failed
+
+let toDoList=[ ["10:30AM","Wake up"],["12:00PM","3701 Lecture"],["3:15pm","WIL meeting"]]
+struct Hstack_list_view:View{
+    var body:some View{
+        List{
+            ForEach(toDoList,id:\.self){
+                todo in HStack{
+                    Text(todo[0]).font(.title2).frame(width:80).background(.green)
+                    Text(todo[1])
+                    Spacer()
+                    Image(systemName: todo[2]).foregroundColor(.blue)
+                    
+                }
+            }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         //ContentView()
         //list_view()
-        foreach_list_view()
-        
+        //foreach_list_view()
+        Hstack_list_view()
     }
 }
---
+++ week2/hstack_in_list_view.swift
@@ -0,0 +1,8 @@
+//
+//  hstack_in_list_view.swift
+//  week2
+//
+//  Created by Wonwoo Choi on 13/3/2023.
+//
+
+import Foundation
