4f1d6a5272f3cbc16b79925034a3d548ad3659e7
wwc <onechoo1106@gmail.com>
2023-03-13 20:29:17 +1000

    create view folder for view file

--- week2/Views/ContentView.swift
+++ week2/Views/ContentView.swift
@@ -96,24 +96,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-
-struct ExtractedView: View {
-    @State var task:[String] // if you want to change variable you need @State
-    var body: some View {
-        HStack{
-            Text(task[0]).font(.title2).frame(width:100).background(.green)
-            Text(task[1])
-            Spacer()
-            Image(systemName: task[2])
-            //Spacer()
-            //Image(systemName: todo[2]).foregroundColor(.blue)// this cause problem but why????
-        }.onTapGesture {
-            if(task[2]=="xmark.circle"){
-                task[2]="checkmark.circle"
-            }else{
-                task[2]="xmark.circle"
-            }
-            //print("\(task[1]) is ticked")
-        }
-    }
-}
---
+++ week2/Views/ExtractedView.swift
@@ -0,0 +1,37 @@
+//
+//  ExtractedView.swift
+//  week2
+//
+//  Created by Wonwoo Choi on 13/3/2023.
+//
+
+import SwiftUI
+
+
+struct ExtractedView: View {
+    @State var task:[String] // if you want to change variable you need @State
+    var body: some View {
+        HStack{
+            Text(task[0]).font(.title2).frame(width:100).background(.green)
+            Text(task[1])
+            Spacer()
+            Image(systemName: task[2])
+            //Spacer()
+            //Image(systemName: todo[2]).foregroundColor(.blue)// this cause problem but why????
+        }.onTapGesture {
+            if(task[2]=="xmark.circle"){
+                task[2]="checkmark.circle"
+            }else{
+                task[2]="xmark.circle"
+            }
+            //print("\(task[1]) is ticked")
+        }
+    }
+}
+
+/*
+struct ExtractedView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExtractedView()
+    }
+}*/
